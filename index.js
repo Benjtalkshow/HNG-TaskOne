5c12900d819a20245d6e72223b818ce2d59df627
Benjtalkshow <chinedubenj@gmail.com>
2023-09-08 02:11:36 -0700

    my first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const currentDate = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
+const btn = document.querySelector('[data-testid="githubURL"]');
+
+const createCurrentDate = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const date = new Date();
+const daysOfTheWeek = createCurrentDate[date.getDay()];
+currentDate.innerHTML = `Day: ${daysOfTheWeek}`;
+
+function displayTime() {
+  setTimeout(updateTimeAndDate, 1000);
+}
+
+function updateTimeAndDate() {
+  const date = new Date();
+  const hour = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getUTCSeconds();
+
+  const timeDisplay = `${hour}:${minutes}:${seconds}`;
+  currentTime.innerHTML = `Time: ${timeDisplay}`;
+  displayTime();
+}
+
+displayTime();
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.open('#', '_blank');
+});
