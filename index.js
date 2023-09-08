3ab04e4efe3b30f824c4b654b1dcd344871faa23
Benjtalkshow <chinedubenj@gmail.com>
2023-09-08 13:02:17 -0700

    changed my html button to link tag, converted time to milliseconds

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const currentDate = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
-const btn = document.querySelector('[data-testid="githubURL"]');
 
 const createCurrentDate = [
   "Sunday",
@@ -15,24 +14,6 @@ const date = new Date();
 const daysOfTheWeek = createCurrentDate[date.getDay()];
 currentDate.innerHTML = `Day: ${daysOfTheWeek}`;
 
-function displayTime() {
-  setTimeout(updateTimeAndDate, 1000);
-}
 
-function updateTimeAndDate() {
-  const date = new Date();
-  const hour = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
-  const seconds = date.getUTCSeconds();
-
-  const timeDisplay = `${hour}:${minutes}:${seconds}`;
-  currentTime.innerHTML = `Time: ${timeDisplay}`;
-  displayTime();
-}
-
-displayTime();
-
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.open('https://github.com/Benjtalkshow/HNG-TaskOne', '_blank');
-});
+const currentTimer = Date.now();
+currentTime.innerHTML = `Time: ${currentTimer}`;
